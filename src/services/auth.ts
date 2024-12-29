f5e1119d18702cc4c3bafdf549b63e746e02baf0
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-30 00:39:00 +0600

    enabled logs for db wake up time calculation

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -4,7 +4,9 @@ import { UserRole } from "@/types/auth";
 import { eq } from "drizzle-orm";
 
 export const getUserData = async (email: string) => {
+  console.time("getUserData");
   const [data] = await db.select().from(auth).where(eq(auth.email, email)).innerJoin(users, eq(users.id, auth.userId));
+  console.timeEnd("getUserData");
 
   if (!data?.users) return null;
 
@@ -25,19 +27,27 @@ export const getUserData = async (email: string) => {
 };
 
 export const createUser = async (data: typeof users.$inferInsert) => {
+  console.time("createUser");
   const [res] = await db.insert(users).values(data).returning();
+  console.timeEnd("createUser");
   return res;
 };
 
 export const createAuth = async (data: typeof auth.$inferInsert) => {
+  console.time("createAuth");
   const [res] = await db.insert(auth).values(data).returning();
+  console.timeEnd("createAuth");
   return res;
 };
 
 export const setResetPasswordCode = async (authId: string, code: string) => {
+  console.time("setResetPasswordCode");
   await db.update(auth).set({ passResetCode: code }).where(eq(auth.id, authId));
+  console.timeEnd("setResetPasswordCode");
 };
 
 export const updatePassword = async (authId: string, hashedPass: string) => {
+  console.time("updatePassword");
   await db.update(auth).set({ hashedPass, passResetCode: null }).where(eq(auth.id, authId));
+  console.timeEnd("updatePassword");
 };
