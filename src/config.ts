720921ce5c350fe9f3e68f49d5e6e18c79893638
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-29 23:50:16 +0600

    fixing config based on environment

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 export const USER_ROLES = ["customer", "seller", "admin"] as const;
 export const TOKEN_AGE = 60 * 60 * 24 * 7; // 7 day
-export const GOOGLE_REDIRECT_URL = "http://localhost:3000/api/auth/callback/google";
+export const GOOGLE_REDIRECT_URL =
+  process.env.NODE_ENV == "development"
+    ? "http://localhost:3000/api/auth/callback/google"
+    : "https://cartbay.vercel.app";
