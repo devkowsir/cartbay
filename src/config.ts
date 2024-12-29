5e174865693d53cb0c3aea8452968e7a95102160
Kawsar Ahmed <72383564+devkowsir@users.noreply.github.com>
2024-12-29 23:53:55 +0600

    fixing callback url

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,4 @@
 export const USER_ROLES = ["customer", "seller", "admin"] as const;
 export const TOKEN_AGE = 60 * 60 * 24 * 7; // 7 day
-export const GOOGLE_REDIRECT_URL =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:3000/api/auth/callback/google"
-    : "https://cartbay.vercel.app";
+export const ORIGIN = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://cartbay.vercel.app";
+export const GOOGLE_REDIRECT_URL = `${ORIGIN}/api/auth/callback/google`;
